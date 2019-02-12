ce8113c96333054d152696f5317e28db72177a9b
zhaojinbiao <zhaojinbiao@geekfinancer.com>
2019-02-12 15:21:35 +0800

    Site updated: 2019-02-12 15:21:35

--- js/search.js
+++ js/search.js
@@ -86,3 +86,11 @@ var searchFunc = function(path, search_id, content_id) {
         }
     })
 }
+
+var inputArea = document.querySelector("#local-search-input");
+var getSearchFile = function(){
+    var path = "/search.xml";
+    searchFunc(path, 'local-search-input', 'local-search-result');
+}
+
+inputArea.onfocus = function(){ getSearchFile() }
